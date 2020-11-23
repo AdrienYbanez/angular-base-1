4a21ca9e410c36638a27be70c3f451da1967d0ad
YbanezAdrien <adrien.ybanez@gmail.com>
2020-11-23 11:57:03 +0100

    Mis en ligne du contenu

---
+++ src/app/exercice2/exercice2.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exercice2',
+  templateUrl: './exercice2.component.html',
+  styleUrls: ['./exercice2.component.css', '../main-component.css']
+})
+export class Exercice2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public afficheConsole() {
+    console.log('click');
+  }
+
+}
